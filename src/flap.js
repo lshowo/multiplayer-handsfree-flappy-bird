6c367a1093b16c03a57859a19730784cdd4ae1a9
Glitch (handsfree-flappy-bird) <none>
2020-10-22 04:45:40 +0000

    📖🍰 Updated with Glitch

--- src/flap.js
+++ src/flap.js
@@ -22,8 +22,8 @@ handsfree.use('flap', {
     
     // Detect up flap
     if (
-      pose.keypoints[9].position.y < pose.keypoints[5].position.y + 40 &&
-      pose.keypoints[10].position.y < pose.keypoints[6].position.y + 40
+      pose.keypoints[9].position.y < pose.keypoints[5].position.y + 100 &&
+      pose.keypoints[10].position.y < pose.keypoints[6].position.y + 100
     ) {
       hasFlappedUp = true
     }
@@ -31,8 +31,8 @@ handsfree.use('flap', {
     // Detect down flap
     if (
       hasFlappedUp &&
-      pose.keypoints[9].position.y > pose.keypoints[5].position.y + 40 &&
-      pose.keypoints[10].position.y > pose.keypoints[6].position.y + 40
+      pose.keypoints[9].position.y > pose.keypoints[5].position.y + 100 &&
+      pose.keypoints[10].position.y > pose.keypoints[6].position.y + 100
     ) {
       hasFlappedUp = false
       bird.flap()
@@ -40,10 +40,11 @@ handsfree.use('flap', {
     
     // Draw Debug info
     ctx.clearRect(0, 0, handsfree.feedback.$debug.width, handsfree.feedback.$debug.height)
+    ctx.fillStyle = '#ff0'
     pose.keypoints.forEach(point => {
       if (point.score > .2) {
         ctx.beginPath()
-        ctx.arc(point.position.x, point.position.y, 10, 0, Math.PI * 2, false)
+        ctx.arc(point.position.x, point.position.y, 20, 0, Math.PI * 2, false)
         ctx.closePath()
         ctx.fill()
       }
