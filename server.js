789bde7909c54dba64f75a4121f1b216b8100f9f
lshowo <34123188+lshowo@users.noreply.github.com>
2022-06-14 15:56:08 +0800

    fixxxxxx

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 // assign our port number to be whatever heroku give us OR the dafault 3000
 var PORT = process.env.PORT || 3000;
-//var serveStatic = require('serve-static')
-const path = require('path')
 const cors = require('cors')
+//const path = require('path')
 const express = require('express'); //引入express框架
 const app = express();  //创建网站服务器
 
 const server = app.listen(PORT, () => { //监听端口
     console.log('My socket server is running at 127.0.0.1:3000')
   });
+
 function listen() {
     const host = server.address().address;
     const port = server.address().port;
     console.log('Example app listening at http://' + host + ':' + port);
 }
+
 //app.use(cors());
 app.use(express.static('public'));
 
@@ -23,7 +24,6 @@ app.use(express.static('public'));
 //app.use(express.static(path.join(__dirname, 'public')));
 
 var io = require('socket.io')(server); //创建服务器io对象
-//var io = require('socket.io')(server, { serveClient: false })
 
 //收到client的连接,
 io.sockets.on('connection',
